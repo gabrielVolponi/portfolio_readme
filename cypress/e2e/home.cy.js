2cae9c2faaf591fd307ccb1d3f4882173f213869
DESKTOP-AIKJGQ0\gabriel <gabriel.volponi@vidibr.com>
2025-04-05 12:56:36 -0300

    testing ci cd

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -48,7 +48,7 @@ describe('Home Page Tests', () => {
 
   it('Validate Responsive Design for Mobile', () => {
     cy.viewport(480, 800); // Simulate mobile screen
-    cy.get('body').should('have.css', 'font-size', '24px'); // Adjust based on your CSS
+    cy.get('body').should('have.css', 'font-size', '24px'); 
     cy.get('.stacks ul').should('have.css', 'flex-direction', 'column');
   });
 
